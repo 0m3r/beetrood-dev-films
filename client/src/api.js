906cd373f135a887339e6afd174ef3082ee1a77d
Alexander Kras'ko <0m3r.mail@gmail.com>
2020-05-28 19:40:13 +0300

    Add signup

--- client/src/api.js
+++ client/src/api.js
@@ -8,4 +8,7 @@ export default {
         update: film => axios.put(`/api/films/${film._id}`, {film}).then(res => res.data.film),
         delete: film => axios.delete(`/api/films/${film._id}`),
     },
+    users:{
+        create: user => axios.post("/api/users", {user}),
+    }
 }
