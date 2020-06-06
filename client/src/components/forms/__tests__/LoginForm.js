47f823ccf82d261cd4fdbed0522c7d0fb25764cb
Alexander Kras'ko <0m3r.mail@gmail.com>
2020-06-06 11:14:54 +0300

    improve test render

--- client/src/components/forms/__tests__/LoginForm.js
+++ client/src/components/forms/__tests__/LoginForm.js
@@ -2,18 +2,42 @@ import React from "react";
 import LoginForm from "../LoginForm";
 import ReactDOM from "react-dom";
 import {MemoryRouter} from "react-router-dom";
-import { toHaveAttribute, toHaveTextContent} from "@testing-library/jest-dom/matchers"
+import { toHaveAttribute, toHaveTextContent} from "@testing-library/jest-dom/matchers";
+import {queries, getQueriesForElement} from "@testing-library/dom";
+
 expect.extend({toHaveAttribute, toHaveTextContent})
 
-test("LoginFrom  should render correct", () => {
-    const div  = document.createElement("div");
-    ReactDOM.render(
-        <MemoryRouter>
-            <LoginForm />
-        </MemoryRouter>
-    , div);
+function render(ui) {
+    const container = document.createElement('div');
+    ReactDOM.render(ui, container);
+    const queries = getQueriesForElement(container)
+    return {...queries, container}
+}
 
-    expect(div.querySelector("#email").type).toBe("email")
-    expect(div.querySelector("#email")).toHaveAttribute('type', "email")
-    expect(div.querySelector("label")).toHaveTextContent("Email")
+// test("LoginFrom  should render correct", () => {
+//     const div  = document.createElement("div");
+//     ReactDOM.render(
+//         <MemoryRouter>
+//             <LoginForm />
+//         </MemoryRouter>
+//     , div);
+//
+//     // expect(div.querySelector("#email").type).toBe("email")
+//     // expect(div.querySelector("#email")).toHaveAttribute('type', "email")
+//     // expect(div.querySelector("label")).toHaveTextContent("Email")
+//
+//     // const emailEl = queries.getByLabelText(div, /email/i);
+//     const {getByLabelText} = render(div);
+//     const emailEl =   getByLabelText(/email/i);
+//     expect(emailEl).toHaveAttribute('type', 'email');
+// })
+
+test("LoginForm should render correct", () => {
+    const { getByLabelText } = render(
+    <MemoryRouter>
+    <LoginForm />
+    </MemoryRouter>
+);
+    const emailEl = getByLabelText(/email/i)
+    expect(emailEl).toHaveAttribute("type", "email");
 })
